df13e990941071ca199cc3716d86b905f97d9759
Caleb Akomaye <calebakomaye@outlook.com>
2025-01-30 10:40:48 +0100

    update app.ts and configure package.json

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,11 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 import router from './routes/routes';
 import { handleSocketConnection } from './controllers/transcribe_controller';
+import path from 'path'
 
 const app = express();
 const server = createServer(app);
-
+const __dirname = path.resolve()
 // Socket.IO server with CORS
 const io = new Server(server, {
   cors: {
@@ -31,6 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 //Socket.IO connections
 io.on('connection', (socket) => {
